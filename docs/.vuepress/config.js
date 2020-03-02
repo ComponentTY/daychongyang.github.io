59e0832ddce5e27b6001b1c671e2ff010ca1b591
Day <daychongyang@gmail.com>
2020-03-02 11:09:25 +0800

    feat: 目录更正

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,65 +1,30 @@
 module.exports = {
-  title: "DayDayUp",
-  description: "Day Day Up!",
-  base: "/",
-  themeConfig: {
-    nav: [
-      { text: "Daily", link: "/daily/" },
-      { text: "JavaScript", link: "/javascript/" },
-      {
-        text: "Links",
-        items: [
-          { text: "前端精读周刊", link: "https://github.com/dt-fe/weekly" }
-        ]
-      }
-    ],
-    sidebar: {
-      "/daily/": [
-        {
-          title: "HTML",
-          path: "/daily/html/",
-          children: ["/daily/html/seo"]
-        },
-        {
-          title: "CSS",
-          path: "/daily/css/",
-          children: ["/daily/css/layout"]
-        },
-        {
-          title: "JavaScript",
-          path: "/daily/javascript/",
-          children: [
-            "/daily/javascript/数据类型",
-            "/daily/javascript/类型转换",
-            "/daily/javascript/类型判断"
-          ]
-        },
-        {
-          title: "React",
-          path: "/daily/react/",
-          children: [
-            {
-              title: "React 源码解析",
-              path: "/daily/react/source",
-              children: ["/daily/react/source/api"]
-            }
-          ]
-        },
-        {
-          title: "React Native",
-          path: "/daily/react-native/",
-          children: [
-            "/daily/react-native/platform-mac-workspace",
-            "/daily/react-native/common-setting"
-          ]
-        },
-        {
-          title: "CI",
-          path: "/daily/ci/",
-          children: ["/daily/ci/travis"]
-        }
-      ]
-    }
-  },
-  plugins: ["@vuepress/medium-zoom"]
-};
+	title: "DayDayUp",
+	description: "Day Day Up!",
+	base: "/",
+	themeConfig: {
+		nav: [
+			{text: "Daily", link: "/daily/"},
+			{text: "JavaScript", link: "/javascript/"},
+			{
+				text: "Links",
+				items: [{text: "前端精读周刊", link: "https://github.com/dt-fe/weekly"}]
+			}
+		],
+		sidebar: {
+			"/javascript/": [
+				{
+					title: "数据类型和数据结构",
+					path: "/javascript/数据类型和数据结构/",
+					children: [
+						"/javascript/数据类型和数据结构/数据结构",
+						"/javascript/数据类型和数据结构/数据类型",
+						"/javascript/数据类型和数据结构/类型转换",
+						"/javascript/数据类型和数据结构/类型判断"
+					]
+				}
+			]
+		}
+	},
+	plugins: ["@vuepress/medium-zoom"]
+}
