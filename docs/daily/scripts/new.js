a84801b1e8c501065511b3e4075f2319c3a6619c
Day <daychongyang@gmail.com>
2020-03-04 11:40:00 +0800

    feat: new

---
+++ docs/daily/scripts/new.js
@@ -0,0 +1,22 @@
+function create() {
+	var current = new Object()
+
+	var Constructor = Array.prototype.shift.call(arguments)
+
+	current.__proto__ = Constructor.prototype
+
+	var result = Constructor.apply(current, arguments)
+
+	return (typeof result === "object" || typeof result === "function") && result !== null ? result : current
+}
+
+function Foo(bar) {
+	this.bar = bar
+	return Symbol()
+}
+
+var foo = create(Foo, "12138")
+console.log(foo)
+
+var foo = new Foo("12")
+console.log(foo)
